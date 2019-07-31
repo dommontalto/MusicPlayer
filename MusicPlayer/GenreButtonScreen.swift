448dc49301ccf9a68793947471d989c55c3dfa01
dommontalto <dominic.montalto@fusionautomation.com>
2019-07-31 10:03:20 +0100

    Music player added

--- MusicPlayer/GenreButtonScreen.swift
+++ MusicPlayer/GenreButtonScreen.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class GenreButtonScreen: UIViewController {
+    
+    var musicPlayer = MPMusicPlayerController.applicationMusicPlayer
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,12 +20,28 @@ class GenreButtonScreen: UIViewController {
     }
     
     @IBAction func genreButtonTapped(_ sender: UIButton) {
+        playGenre(genre: sender.currentTitle!)
     }
     
     @IBAction func stopButtonTapped(_ sender: UIButton) {
+        musicPlayer.stop()
     }
     
     @IBAction func nextButtonTapped(_ sender: UIButton) {
+        musicPlayer.skipToNextItem()
+    }
+    
+    func playGenre(genre: String){
+        
+        musicPlayer.stop()
+        
+        let query = MPMediaQuery()
+        let predicate = MPMediaPropertyPredicate(value: genre, forProperty: MPMediaItemPropertyGenre)
+        
+        query.addFilterPredicate(predicate)
+        musicPlayer.setQueue(with: query)
+        musicPlayer.shuffleMode = .songs
+        musicPlayer.play()
     }
     
 }
