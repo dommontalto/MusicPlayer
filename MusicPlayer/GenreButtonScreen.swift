61c669c839a9c5efb030ec11c017098f858c6090
dommontalto <dominic.montalto@fusionautomation.com>
2019-07-31 09:54:54 +0100

    General layout and buttons added

---
+++ MusicPlayer/GenreButtonScreen.swift
@@ -0,0 +1,28 @@
+//
+//  GenreButtonScreen.swift
+//  MusicPlayer
+//
+//  Created by Software Development on 31/07/2019.
+//  Copyright © 2019 Software Development. All rights reserved.
+//
+
+import UIKit
+
+class GenreButtonScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+    
+    @IBAction func genreButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+    }
+    
+}
